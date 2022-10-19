a9e193f7895332d8b80ab5a9679839deb7038e73
ipucheta110962 <colopucheta@gmail.com>
2022-10-19 20:45:48 -0300

    Final code

--- features/cart_validation.feature
+++ features/cart_validation.feature
@@ -3,17 +3,13 @@ Feature: Cart Validation
   Background: Preconditions
     Given I launch the browser
     When I open the Web Application
-    And I login with "<username>" and "<password>"
+    And I enter username "standard_user" and "secret_sauce"
+    Then I log in
 
-  Scenario: Logged in
-    Then I am logged in
-
-  Scenario: Cart counter
+  Scenario: Cart functionality
     Given I am in Inventory Page
     When I add an item to cart
     Then verify that the cart counter increments
+    And I navigate to the Cart Page
+    And verify that the Cart Page contains the selected items
 
-  Scenario: Cart Items
-    Given I have items added to cart
-    When I navigate to the Cart Page
-    Then verify that the Cart Page contains the selected items
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,6 +1,9 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @given('I launch the browser')
 def open_browser(context):
@@ -12,41 +15,42 @@ def navigate_to_web(context):
     context.driver.get("https://www.saucedemo.com/")
 
 
-@when('enter valid credentials')
-def enter_credentials(context):
-    username = context.driver.find_element(By.ID("user-name"))
-    username.
+@when('I enter username "{username}" and "{password}"')
+def enter_credentials(context,username,password):
+    context.driver.find_element(By.ID, "user-name").send_keys(username)
+    context.driver.find_element(By.ID, "password").send_keys(password)
 
 
-@then('the user is logged in')
+@then('I log in')
 def click_login(context):
+    context.driver.find_element(By.ID, "login-button").click()
 
 
+@given('I am in Inventory Page')
+def verify_user_is_in_inventory(context):
+    title = context.driver.find_element(By.ID, "inventory_container").is_displayed()
+    assert title is True
 
-@given('the user is in Inventory Page')
-def navigate_to_inventory(context):
-
-
-@when('the user adds an item to cart')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When the user adds an item to cart')
+@when('I add an item to cart')
+def add_to_cart(context):
+    context.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
 
 
 @then('verify that the cart counter increments')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then verify that the cart counter increments')
+def verify_cart_counter_increment(context):
+    counter = context.driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert counter.text == "1"
+    return counter
 
 
-@given('the user has items added to cart')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the user has items added to cart')
-
-
-@when('the user navigates to the Cart Page')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When the user navigates to the Cart Page')
+@then('I navigate to the Cart Page')
+def navigate_to_cart(context):
+    context.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 
 
 @then('verify that the Cart Page contains the selected items')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then verify that the Cart Page contains the selected items')
+    quantity = context.driver.find_element(By.CLASS_NAME, "cart_quantity")
+    item_name = context.driver.find_element(By.CLASS_NAME, "inventory_item_name")
+    assert quantity.text == "1"
+    assert item_name.text == "Sauce Labs Backpack"
